65793ef4b8adb2b305e9b791f0ddfbc796e28727
Ananyobrata Pal <ananyo141@gmail.com>
2023-03-12 02:02:25 +0530

    Open login page every time app is opened

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_shala/views/logoptions.dart';
 import 'constants/routes.dart';
-import 'views/main_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // final cameras = await availableCameras();
 
   // get login instance
-  final SharedPreferences sharedPreferences =
-      await SharedPreferences.getInstance();
-  var accessToken = sharedPreferences.getString('access');
-
   // // Get a specific camera from the list of available cameras.
   // final firstCamera = cameras.first;
   runApp(MaterialApp(
@@ -29,9 +23,7 @@ Future<void> main() async {
         child: Image.asset('images/logo.png'),
       ),
       // if logged in, redirect to main page, else to login page
-      nextScreen: (accessToken != null && accessToken.isNotEmpty)
-          ? const MainPage()
-          : const PrevLogin(),
+      nextScreen: const PrevLogin(),
     ),
     onGenerateRoute: RouteGenerator.generateRoute,
   ));
